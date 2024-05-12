2f8698f9484792ec24fbe0857abbbf89f304a536
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-11 19:26:53 -0500

    Se añade el navbar

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EnumVariablesGlobales } from 'src/app/enums/EnumVariablesGlobales';
+import { VariablesGlobalesService } from 'src/app/services/variables-globales.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  public titulo: string = '';
 
+  constructor(private obser: VariablesGlobalesService) {}
+
+  public ngOnInit(): void {
+    this.obser
+      .getData(EnumVariablesGlobales.TITULO_NAVBAR)
+      .subscribe((value: string) => {
+        this.titulo = value;
+      });
+  }
 }
