9322eeb4f9b547ec09cca60a8006a23e54bc7ae1
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-11 19:27:08 -0500

    Se añaden los estimlos de la pagina de consultas

--- src/app/pages/consulta/consulta.component.ts
+++ src/app/pages/consulta/consulta.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EnumVariablesGlobales } from 'src/app/enums/EnumVariablesGlobales';
+import { VariablesGlobalesService } from 'src/app/services/variables-globales.service';
 
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
-  styleUrls: ['./consulta.component.css']
+  styleUrls: ['./consulta.component.css'],
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
+  constructor(private obser: VariablesGlobalesService) {}
 
+  public ngOnInit(): void {
+    this.obser.setData(
+      EnumVariablesGlobales.TITULO_NAVBAR,
+      'Consulta de documento'
+    );
+  }
 }
