f542985457209cf500296ea92a87525e7d854d32
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-12 10:39:39 -0500

    Se muestra la informacion consultada en la pantalla de informacion

--- src/app/pages/informacion/informacion.component.ts
+++ src/app/pages/informacion/informacion.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EnumTipoDocumento } from 'src/app/enums/EnumTipoDocumento';
+import { EnumVariablesGlobales } from 'src/app/enums/EnumVariablesGlobales';
+import { IUsuario } from 'src/app/interfaces/IUsuario';
+import { VariablesGlobalesService } from 'src/app/services/variables-globales.service';
 
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.component.html',
-  styleUrls: ['./informacion.component.css']
+  styleUrls: ['./informacion.component.css'],
 })
-export class InformacionComponent {
+export class InformacionComponent implements OnInit {
+  public datosUsuario: IUsuario = null as any;
 
+  constructor(private obser: VariablesGlobalesService) {}
+
+  public ngOnInit(): void {
+    this.obser.setData(EnumVariablesGlobales.TITULO_NAVBAR, 'Información');
+    this.consultarDatos();
+  }
+
+  private consultarDatos(): void {
+    this.obser
+      .getData(EnumVariablesGlobales.DATOS_USUARIO)
+      .subscribe((res: IUsuario) => {
+        let tiposDocumento: { [key: string]: string } = EnumTipoDocumento;
+        let tipoDocumento: string = '';
+        for (const key of Object.keys(tiposDocumento)) {
+          if (key === res.tipoDocumento) {
+            tipoDocumento = tiposDocumento[key];
+            break;
+          }
+        }
+        this.datosUsuario = { ...res, tipoDocumento };
+      });
+  }
 }
