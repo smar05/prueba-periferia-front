d5e931d988f83346fc5edc246a0d6496a1bad328
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-12 14:25:58 -0500

    Pruebas unitarias a las paginas

--- src/app/pages/informacion/informacion.component.spec.ts
+++ src/app/pages/informacion/informacion.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InformacionComponent } from './informacion.component';
+import { EnumVariablesGlobales } from 'src/app/enums/EnumVariablesGlobales';
+import { from, of } from 'rxjs';
+import { EnumRutas } from 'src/app/enums/EnumRutas';
 
 describe('InformacionComponent', () => {
   let component: InformacionComponent;
@@ -8,7 +11,7 @@ describe('InformacionComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [InformacionComponent]
+      declarations: [InformacionComponent],
     });
     fixture = TestBed.createComponent(InformacionComponent);
     component = fixture.componentInstance;
@@ -17,5 +20,39 @@ describe('InformacionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit', function () {
+    const obserSpy = spyOn(component['obser'], 'setData');
+    spyOn(component as any, 'consultarDatos').and.returnValue(null);
+
+    component.ngOnInit();
+
+    expect(obserSpy).toHaveBeenCalledWith(
+      EnumVariablesGlobales.TITULO_NAVBAR,
+      'Información'
+    );
+  });
+
+  it('consultarDatos', function () {
+    const mockData = { tipoDocumento: 'C', name: 'John Doe' };
+    const obserSpy = spyOn(component['obser'], 'getData').and.returnValue(
+      of(mockData)
+    );
+
+    component['consultarDatos']();
+
+    expect(obserSpy).toHaveBeenCalledWith(EnumVariablesGlobales.DATOS_USUARIO);
+    expect(component.datosUsuario).toBeDefined();
+  });
+
+  it('consultarDatos - error', function () {
+    const obserSpy = spyOn(component['obser'], 'getData').and.returnValue(
+      from(Promise.reject())
+    );
+
+    component['consultarDatos']();
+
+    expect(obserSpy).toHaveBeenCalledWith(EnumVariablesGlobales.DATOS_USUARIO);
   });
 });
